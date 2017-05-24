a34c1190040b39f2df2c8738e1c0b193531fdf92
George V <george.vaneaton@medhost.com>
2017-05-24 12:26:57 -0500

    learning C

---
+++ SoloLearn/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   /* cout << "Hello world!" << endl;
+
+    cout << "This " << "is " << "awesome!" << endl; */
+
+   // cout << "I love programming!!! \n" << endl ;
+   /* cout << "moved to a new line \n\n";
+    cout << "not moved to a new line";
+
+    cout << "I \n love \n C++"; */
+
+
+ /*   int myVariable = 10;
+    cout << myVariable;
+
+    int a= 30;
+    int b = 15;
+    int sum = a+b;
+    cout << sum;
+
+    int num;
+    cout << "please enter a number \n";
+    cin >> num; */
+
+  /*nt a, b;
+    int sum;
+    cout <<"enter a number \n";
+    cin>> a;
+    cout << "enter a new number \n";
+    cin >>b;
+    cout << "a " << a << " or b "<<b<< endl;
+    sum = a + b;
+    cout << "sum is " << sum << endl;
+*/
+   /*nt x = 25 %7;
+    cout <<x;
+*/
+int a=3;
+int b=2;
+b=a++;
+cout<<++b;
+
+
+    return 0;
+}
